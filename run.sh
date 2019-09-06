6c53411c822615f783c7272b86cf33f9539366aa
Rakesh R Menon <rrmenon@cs.umass.edu>
2019-09-06 15:30:14 -0400

    small fix

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python main_3d.py --checkpoint_path=/projects/katefgroup/yunchu/testcar1_multi/checkpoint_950/ --mesh=car1 --num-rollouts=5 --num-iterations=1000
+python -W ignore main_3d.py --checkpoint_path=/projects/katefgroup/yunchu/testcar1_multi/checkpoint_950/ --mesh=car1 --num-rollouts=5 --num-iterations=1000
